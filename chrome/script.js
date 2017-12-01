75caa2870442464b39ba5ed26fb98772dedf9e1d
Pedro Markun <pedro@markun.com.br>
2017-12-01 17:00:00 -0200

    reestruturando código para ficar mais rapido

--- chrome/script.js
+++ chrome/script.js
@@ -7,53 +7,28 @@ function processaCamara(data) {
       text: " (UNCENSORED)",
       cursor:false
    });
-    for (i in data.records) {
-      v = data.records[i]
-        if (DEBUG) {
-          console.log("Checking for " + v[1]);
-        }
-      
-        el = $("iframe.external-content-iframe").contents().find("td.nome_valor:contains('"+v[0]+"')")
-        el.text("")
-        el.typist({
-            speed:12,
-            text: v[1],
-            cursor: false
-        })
-      }
+
+   $("iframe.external-content-iframe").contents().find("td.nome_valor").each(function () {
+    var matricula = $(this).text().trim()
+    if (data[matricula]) {
+      $(this).text(data[matricula]['nome'])
+    }
+
+   });
   });
 }
 
 function processaAlesp(data) {
     jQuery(document).ready(function($) {
-/*   $("#explicacao_salario").typist({
-      speed:12,
-      text: " (UNCENSORED)",
-      cursor:false
-   });
-*/
-    for (i in data.records) {
-      v = data.records[i]
-        if (DEBUG) {
-          console.log("Checking for " + v[1]);
-        }
-      
-        var regex = new RegExp('^'+v[0]+'$')
-        console.log(regex)
-        el = $("td[width='16%']").filter(function () {
-          return regex.test($(this).text()); 
-        });
-        console.log(el)
-        
-        el.text("")
-        el.typist({
-            speed:12,
-            text: v[1],
-            cursor: false
-        })
+
+    $("td[width='16%']").each(function () {
+      var matricula = $(this).text().trim()
+      if (data[matricula]) {
+        $(this).text(data[matricula]['nome'])
       }
-  });
-}
+      });
+    });
+  }
 
 
 //Carrega dados e roda código na página
@@ -65,7 +40,12 @@ if (url.indexOf("camara.sp.gov.br") > -1) {
       dialect: {}
     }
   ).done(function(dataset) { 
-    processaCamara(dataset);
+    var data = {}
+    for (i in dataset.records) {
+      v = dataset.records[i]
+      data[v[0]] = { "nome" : v[1]}
+    }
+    processaCamara(data);
   });
 }
 
@@ -75,7 +55,13 @@ if (url.indexOf("al.sp.gov.br") > -1) {
     dialect: {}
   }
   ).done(function(dataset) { 
-    processaAlesp(dataset);
+
+    var data = {}
+    for (i in dataset.records) {
+      v = dataset.records[i]
+      data[v[0]] = { "nome" : v[1]}
+    }
+    processaAlesp(data);
   });
 }
 
