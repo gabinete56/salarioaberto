700acb019f9ee7fbf5e55b60513aaf9974f17aec
Pedro Markun <pedro@markun.com.br>
2017-12-01 13:31:12 -0200

    aguarda os dados carregarem antes de checar a página, todo: verificar se tem como carregar os dados na memória uma só vez

--- chrome/script.js
+++ chrome/script.js
@@ -1,29 +1,32 @@
+function processaPagina(data) {
+    jQuery(document).ready(function($) {
+   $(".page-title").typist({
+      speed:12,
+      text: " (UNCENSORED)",
+      cursor:false
+   });
+    for (i in data.records) {
+      v = data.records[i]
+        if (DEBUG) {
+          console.log("Checking for " + v[1]);
+        }
+      
+        el = $("iframe.external-content-iframe").contents().find("td.nome_valor:contains('"+v[0]+"')")
+        el.text("")
+        el.typist({
+            speed:12,
+            text: v[1],
+            cursor: false
+        })
+      }
+  });
+}
+
+//Carrega dados e roda código na página
 var data = CSV.fetch({
     url: chrome.extension.getURL('data/camara.csv'),
     dialect: {}
   }
-).done(function(dataset) {
- //checar se o arquivo foi carregado
-});
-
-
-jQuery(document).ready(function($) {
-  var banner = $("<h2 style='color:red'>SAIBA QUEM É QUEM NOS SALÁRIOS DA CÂMARA</h2>")
- $(".page-title").typist({
-    speed:12,
-    text: " (UNCENSORED)",
-    cursor:false
- });
-
-  for (i in data.records) {
-    v = data.records[i]
-      console.log("Checking for " + v[1]);
-      el = $("iframe.external-content-iframe").contents().find("td.nome_valor:contains('"+v[0]+"')")
-      el.text("")
-      el.typist({
-          speed:12,
-          text: v[1],
-          cursor: false
-      })
-    }
+).done(function(dataset) { 
+  processaPagina(dataset);
 });
