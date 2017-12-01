37732ab8cc791ef759f304ce5454bf69239b3ee2
Pedro Markun <pedro@markun.com.br>
2017-12-01 15:57:08 -0200

    adicionando suporte a alesp, vale checar performance

--- chrome/script.js
+++ chrome/script.js
@@ -1,4 +1,6 @@
-function processaPagina(data) {
+DEBUG = true
+
+function processaCamara(data) {
     jQuery(document).ready(function($) {
    $(".page-title").typist({
       speed:12,
@@ -22,11 +24,58 @@ function processaPagina(data) {
   });
 }
 
+function processaAlesp(data) {
+    jQuery(document).ready(function($) {
+/*   $("#explicacao_salario").typist({
+      speed:12,
+      text: " (UNCENSORED)",
+      cursor:false
+   });
+*/
+    for (i in data.records) {
+      v = data.records[i]
+        if (DEBUG) {
+          console.log("Checking for " + v[1]);
+        }
+      
+        var regex = new RegExp('^'+v[0]+'$')
+        console.log(regex)
+        el = $("td[width='16%']").filter(function () {
+          return regex.test($(this).text()); 
+        });
+        console.log(el)
+        
+        el.text("")
+        el.typist({
+            speed:12,
+            text: v[1],
+            cursor: false
+        })
+      }
+  });
+}
+
+
 //Carrega dados e roda código na página
-var data = CSV.fetch({
-    url: chrome.extension.getURL('data/camara.csv'),
+var url = location.href
+
+if (url.indexOf("camara.sp.gov.br") > -1) {
+  var data = CSV.fetch({
+      url: chrome.extension.getURL('data/camara.csv'),
+      dialect: {}
+    }
+  ).done(function(dataset) { 
+    processaCamara(dataset);
+  });
+}
+
+if (url.indexOf("al.sp.gov.br") > -1) {
+  var data = CSV.fetch({
+    url: chrome.extension.getURL('data/alesp.csv'),
     dialect: {}
   }
-).done(function(dataset) { 
-  processaPagina(dataset);
-});
+  ).done(function(dataset) { 
+    processaAlesp(dataset);
+  });
+}
+
