72a7fb9527113b8124456b804c6b88971c681c3c
Pedro Markun <pedro@markun.com.br>
2017-12-05 17:21:47 -0200

    adicionando tooltip que avisa das matriculas nao encontradas

--- chrome/script.js
+++ chrome/script.js
@@ -5,7 +5,10 @@ function processaCamara(data) {
     var matricula = $(this).text().trim()
     if (data[matricula]) {
       $(this).text(data[matricula]['nome'])
+      } else {
+        $(this).html(matricula+"<a href='#' title='Ainda não conseguimos identificar essa matricula. Se for você, ajuda a gente? :)'>???</a>")
       }
+
      });
     return true;
 }
@@ -24,7 +27,7 @@ function processaAlesp(data) {
 //Carrega dados e roda código na página
 var url = location.href
 
-var aparecium = $("<div id='aparecium' style='display:none;min-height:20px;width:100%;color:#fff;background-color:#af1352;padding:5px;text-align:center;font-weight:bold;'>Com o Plugin Aparecium Salários da <a style='color:#D0D820;text-decoration:none;' href='http://www.minhasampa.org.br'>Minha Sampa</a> + <a style='color:#D0D820;text-decoration:none;' href='http://facebook.com/gabinete56'>Gabinete 56</a> nenhuma Assembleia ou Câmara ficará secreta.</div>")
+var aparecium = $("<div id='aparecium' style='display:none;min-height:20px;width:100%;color:#fff;background-color:#af1352;padding:5px;text-align:center;font-weight:bold;'>Com o Plugin Aparecium Salários da <a style='color:#D0D820;text-decoration:none;' href='http://www.minhasampa.org.br' target='_blank'>Minha Sampa</a> + <a style='color:#D0D820;text-decoration:none;' href='http://facebook.com/gabinete56' target='_blank'>Gabinete 56</a> nenhuma Assembleia ou Câmara ficará secreta.</div>")
 
 if (url.indexOf("camara.sp.gov.br") > -1) {
   var data = CSV.fetch({
